64491cd71fc6db9ca26fe4466fa49e872e6914e1
eugenetedkim <eugenetedkim@gmail.com>
2023-12-09 12:24:24 -0800

    Created Hero component and added it to the main entry point of the application

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+      {/* Overlay */}
+      <div />
+      <div>
+        <h2>Heading</h2>
+        <p>Message</p>
+        <button>Book</button>
+      </div>
+    </div>
   )
 }
 
