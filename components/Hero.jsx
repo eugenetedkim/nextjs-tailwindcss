d2227a741767ab58d26aa0227e54a076a215ab47
eugenetedkim <eugenetedkim@gmail.com>
2023-12-17 13:24:50 -0800

    Replaced image

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>
+    <div class="relative w-full h-screen overflow-hidden">
       <Image
         src="/images/convictlake.jpg"
         fill
@@ -10,7 +10,12 @@ const Hero = () => {
         objectPosition="left center"
         alt=""
       />
-      
+      {/* Overlay */}
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-white text-center">
+        <h2>Heading</h2>
+        <p>Message</p>
+        <button>Book</button>
+      </div>
     </div>
   );
 };
