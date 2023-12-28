b5249047d1dfc530d8b5d87a6657902a8eea7cfe
eugenetedkim <eugenetedkim@gmail.com>
2023-12-27 22:02:02 -0800

    Add Navbar and mobile friendly functionality

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero heading="Eugene Kim" message="I think, therefore I am."/>
+      <Hero heading="Hi, I'm Eugene. A Software Engineer." message="I solve problems through code, design, and critical thinking."/>
     </>
   )
 }
